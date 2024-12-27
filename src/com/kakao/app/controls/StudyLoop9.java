3c4db1146a8e36000513cc0f7366ab42094c64bf
IncheonPark <dnehd8@gmail.com>
2024-12-27 17:25:28 +0900

    Loop3

--- src/com/kakao/app/controls/StudyLoop9.java
+++ src/com/kakao/app/controls/StudyLoop9.java
@@ -24,38 +24,57 @@ public class StudyLoop9 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		//1번 사냥, 2번 종료
+//		//1번 사냥, 2번 종료
+//		int gold = 0;
+//		//-----------------------------------------------------
+//		for (int i = 1; i <= 15; i++) {
+//						
+//			if (i > 1) {
+//				System.out.println("---------------------------------");
+//				
+//				if (i % 5 == 0) {
+//					gold = gold + (i/5 * 1000);
+//					System.out.println("5레벨씩 상승할 때마다 보너스 골드 지급! - 지급량 : " + (i/5 * 1000) + "골드");
+//				}
+//				System.out.println("레벨업! 현재 레벨 : " + i + " / 현재 골드 : " + gold);
+//				//-----------------------------------------------------
+//				if (i == 15) {
+//					break;
+//				}
+//			}
+//			
+//			System.out.println("1. 몬스터 사냥 / 2. 게임 종료 ");
+//			int menu = sc.nextInt();
+//			
+//			if (menu == 1) {
+//				for (int j = 0; j < i * 3; j++) {
+//					System.out.println(j+1+"마리 몬스터를 사냥했습니다.");
+//					
+//				}
+//			} else {				
+//				break;
+//			}
+//			
+//		} System.out.println("게임을 종료합니다.");
+		
+		int level = 1;
 		int gold = 0;
-		//-----------------------------------------------------
-		for (int i = 1; i <= 15; i++) {
-						
-			if (i > 1) {
-				System.out.println("---------------------------------");
-				
-				if (i % 5 == 0) {
-					gold = gold + (i/5 * 1000);
-					System.out.println("5레벨씩 상승할 때마다 보너스 골드 지급! - 지급량 : " + (i/5 * 1000) + "골드");
-				}
-				System.out.println("레벨업! 현재 레벨 : " + i + " / 현재 골드 : " + gold);
-				//-----------------------------------------------------
-				if (i == 15) {
-					break;
-				}
+		
+		for(level = 1; level < 15; level++) {
+			
+			for (int mon = 1; mon <=3; mon++) {
+				System.out.println(mon + "마리 사냥 성공.");
 			}
 			
-			System.out.println("1. 몬스터 사냥 / 2. 게임 종료 ");
-			int menu = sc.nextInt();
+			System.out.println("축 레벨업!");
 			
-			if (menu == 1) {
-				for (int j = 0; j < i * 3; j++) {
-					System.out.println(j+1+"마리 몬스터를 사냥했습니다.");
-					
-				}
-			} else {				
-				break;
+			if((level+1)%5 == 0) {
+				gold = gold + (level+1) /5 * 1000;
 			}
 			
-		} System.out.println("게임을 종료합니다.");		
+		}
+		System.out.println("현재 레벨 : " + level);
+		System.out.println("현재 골드 : " + gold);
 
 	}
 
